c639a91623706e78d37a3548d865f09cdc9b166d
kennlebu <kenlebu@gmail.com>
2016-09-30 19:44:50 +0300

    -Added AsyncTask to fetch weather data and display it on the list

---
+++ app/src/main/java/com/lebudev/weatherapp/helpers/AppConstants.java
@@ -0,0 +1,9 @@
+package com.lebudev.weatherapp.helpers;
+
+/**
+ * Created by Administrator on 9/26/2016.
+ */
+
+public class AppConstants {
+    public static final String API_KEY = "fe962bb04ab6f9d7e045f198f7e2ce19";
+}
